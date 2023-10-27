e43c9bb38ab405d910a12a7adb5a74c7335edf9e
PatrikRasch <patrikrask@hotmail.com>
2023-10-27 18:34:20 +0200

    feat: connect to MongoDB database for read and write

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,39 @@
 var express = require("express");
 var router = express.Router();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
+const dbUrl = process.env.MONGODB_URL;
+
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+const messageSchema = new mongoose.Schema({
+  text: String,
+  user: String,
+  added: Date,
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+const messages = [];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("message", { title: "Express", messages: messages });
+  Message.find()
+    .then((messages) => {
+      res.render("message", { title: "Express", messages: messages });
+    })
+    .catch((err) => {
+      console.error("Error fetching messages from MongoDB:", err);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
 router.get("/new", function (req, res, next) {
@@ -24,8 +41,20 @@ router.get("/new", function (req, res, next) {
 });
 
 router.post("/new", (req, res) => {
-  messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
-  res.redirect("/");
+  const newMessage = new Message({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date(),
+  });
+  newMessage
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      console.error("Error saving message to MongoDB:", error);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
 module.exports = router;
