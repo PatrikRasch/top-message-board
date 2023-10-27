44302ab378befdb87d98caa34b717d37343cc70c
PatrikRasch <patrikrask@hotmail.com>
2023-10-27 13:55:35 +0200

    feat: add ability to post and read messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("message", { title: "Express", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { heading: "heading", author: "author", message: "message" });
+});
+
+router.post("/new", (req, res) => {
+  messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
